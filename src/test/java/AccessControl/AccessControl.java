2c7e776bfa375589839d5d37d205d37aacee4e81
PaulaJucan <paulici_84@yahoo.com>
2022-06-02 20:19:40 +0300

    ceva mesaj

---
+++ src/test/java/AccessControl/AccessControl.java
@@ -0,0 +1,21 @@
+package AccessControl;
+
+public class AccessControl {
+    //Public=vizibil pentru toata lumea
+    //Private=nu putem scoate informatii din clasa
+    //Protected=aproape public pt toata lumea
+    //Daca vorbim de mostenire,protected e vizibil in acelasi pachet sau pachete diferite
+    //Daca vorbim de obiect, este vizil doar in acelasi pachet
+
+    public void metodaPublica(){
+
+    }
+
+    private void metodaPrivata(){
+
+    }
+
+    protected void metodaProtected(){
+
+    }
+}
